85211c2445ce2cdeffeec79120a6f1de1139cea2
João Ferreira <joao.d.n.ferreira@tecnico.ulisboa.pt>
2025-12-03 23:22:34 +0000

    feat: added TCP connection between Auth and Gateway

--- apps/authentication/src/main.ts
+++ apps/authentication/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthenticationModule } from './authentication.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthenticationModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthenticationModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: 3001,
+      },
+    },
+  );
+  await app.listen();
+  console.log('Authentication microservice started');
 }
 bootstrap();
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -3,6 +3,8 @@ import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(3000);
+  console.log('Gateway is listening for HTTP requests on port 3000');
 }
+
 bootstrap();
