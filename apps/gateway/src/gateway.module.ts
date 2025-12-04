a551b0859d1b765e1bf1084664fcd15b9b91388d
João Ferreira <joao.d.n.ferreira@tecnico.ulisboa.pt>
2025-12-04 22:28:50 +0000

    fix(Logger): fixed issue with logging module in gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { LoggerModule } from '@app/core';
 
 @Module({
   imports: [
+    LoggerModule,
     ClientsModule.register([
       {
         name: 'AUTH_SERVICE',
